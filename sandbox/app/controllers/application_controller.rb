780b21defb5cc11ac13e7a968bfc9e271ea00171
Wen Zhang <zhangwen@cs.berkeley.edu>
2021-12-07 09:46:51 -0800

    no URL pattern

--- sandbox/app/controllers/application_controller.rb
+++ sandbox/app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def set_blockaid_params
     # The user ID is set in `spree_auth_devise`.
-    ActiveRecord::Base.connection.execute("SET @_STORE_URL_PATTERN = '%#{request.env['SERVER_NAME']}%'")
+    # ActiveRecord::Base.connection.execute("SET @_STORE_URL_PATTERN = '%#{request.env['SERVER_NAME']}%'")
     ActiveRecord::Base.connection.execute("SET @_TOKEN = '#{cookies.signed[:token]}'")
     yield
   ensure
